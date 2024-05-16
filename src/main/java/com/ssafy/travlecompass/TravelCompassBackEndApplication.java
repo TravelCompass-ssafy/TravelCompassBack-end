a2a576d891e136b79a970287449b8220afc0c9c3
kijen723 <kij2646@gmail.com>
2024-05-16 09:52:17 +0900

    Create Project

---
+++ src/main/java/com/ssafy/travlecompass/TravelCompassBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.travlecompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCompassBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelCompassBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/travlecompass/TravelCompassBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.travlecompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelCompassBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
