9a3020778bd2c5b9de52ec3216e5c0e4a8a7bace
Sanizzang <tksl4732@gmail.com>
2024-05-20 10:39:55 +0900

    feat: merge

--- src/main/java/com/ssafy/travelcompass/trip/model/service/TripService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.travelcompass.trip.model.service;
-
-import com.ssafy.travelcompass.trip.model.dto.trip.TripDetailDto;
-
-public interface TripService {
-	void registTripDetail(TripDetailDto tripDetailDto);
-}
